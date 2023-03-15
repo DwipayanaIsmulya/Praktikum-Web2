7c366e03f484008a85a367e14b4a9deff8ab1fce
DwipayanaIsmulya <69786097+DwipayanaIsmulya@users.noreply.github.com>
2023-03-15 15:49:30 +0800

    Update PRAK103.php

--- pertemuan1/PRAK103.php
+++ pertemuan1/PRAK103.php
@@ -5,7 +5,7 @@ $fahrenheit = (9/5) * $celcius + 32;
 $reamur = (4/5) * $celcius;
 $kelvin = $celcius + 273.15;
 
-echo "$fahrenheit<br>";
-echo "$reamur<br>";
-echo "$kelvin";
+echo "Fahrenheit (F) : $fahrenheit<br>";
+echo "Reamur (R) : $reamur<br>";
+echo "Kelvin (K): $kelvin";
 ?>
